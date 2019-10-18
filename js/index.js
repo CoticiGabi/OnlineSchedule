138d7367932de6a4350d9381671d2941ce145d53
CoticiGabi <coticigeorgegabriel@yahoo.com>
2019-10-18 10:44:43 +0300

    Canvas Grid

--- js/index.js
+++ js/index.js
@@ -1,30 +1,54 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+let sbt = document.getElementById("submitButton");
+
 
 // Set the canvas width and height according to the display size
 ctx.canvas.width = window.innerWidth*0.9;
 ctx.canvas.height = window.innerHeight/1.3;
 
-// Draw the first horizontal line 
-ctx.moveTo(0, ((ctx.canvas.width) / 13) * 0.6);
-ctx.lineTo(window.innerWidth, ((ctx.canvas.width) / 13) * 0.6);
-ctx.stroke();
+Draw(5, 12);
 
-// Divide each column and row equally
-var newWidth = (ctx.canvas.width) / 13;
-var newHeight = (ctx.canvas.height - ((ctx.canvas.width) / 13) * 0.6) / 5;
 
-// Draw lines vertically
-for (var i = 1; i < 13; i ++) {
-    ctx.moveTo(newWidth * i, 0);
-    ctx.lineTo(newWidth * i, ctx.canvas.height);
+// Clear canvas and draw schedule according to number of days and hours
+sbt.addEventListener("click", function() {
+    let days = document.getElementById("daysInput").value;
+    let hours = document.getElementById("hoursInput").value;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+    Draw(days, hours);
+});
+
+
+// Functions that draws the lines inside the canvas
+function Draw(nrOfDays, nrOfHours) {
+    // Draw the first horizontal line 
+    ctx.moveTo(0, ((ctx.canvas.height) / nrOfDays) * 0.4);
+    ctx.lineTo(window.innerWidth, ((ctx.canvas.height) / nrOfDays) * 0.4);
     ctx.stroke();
-}
 
-// Draw lines horizontally
-for (var i = 1; i <= 5; i ++) {
-    ctx.moveTo(0,newHeight * i + ((ctx.canvas.width) / 13) * 0.6);
-    ctx.lineTo(ctx.canvas.width,newHeight * i + ((ctx.canvas.width) / 13) * 0.6);
+    // Draw the first vertical line
+    ctx.moveTo(((ctx.canvas.width) / nrOfHours) * 0.7, 0);
+    ctx.lineTo(((ctx.canvas.width) / nrOfHours) * 0.7, ctx.canvas.height);
     ctx.stroke();
-}
 
+
+    // Divide each column and row equally
+    let newWidth = (ctx.canvas.width - ((ctx.canvas.width) / nrOfHours) * 0.7) / nrOfHours;
+    let newHeight = (ctx.canvas.height - ((ctx.canvas.height) / nrOfDays) * 0.4) / nrOfDays;
+
+    // Draw lines vertically
+    for (let i = 1; i < nrOfHours; i ++) {
+        ctx.moveTo(newWidth * i + ((ctx.canvas.width) / nrOfHours) * 0.7, 0);
+        ctx.lineTo(newWidth * i + ((ctx.canvas.width) / nrOfHours) * 0.7, ctx.canvas.height);
+        ctx.stroke();
+    }
+
+    // Draw lines horizontally
+    for (let i = 1; i <= nrOfDays; i ++) {
+        ctx.moveTo(0,newHeight * i + ((ctx.canvas.height) / nrOfDays) * 0.4);
+        ctx.lineTo(ctx.canvas.width,newHeight * i + ((ctx.canvas.height) / nrOfDays) * 0.4);
+        ctx.stroke();
+    }
+
+}
