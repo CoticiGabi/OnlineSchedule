5e507f4abb6dd95262badddc401f0d389cb4b445
CoticiGabi <coticigeorgegabriel@yahoo.com>
2019-10-23 18:49:47 +0300

    Added day name to days column

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let dayList = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
 window.onload = function () {
     let hours;
     let days;
@@ -7,9 +9,10 @@ window.onload = function () {
     form.onsubmit = function () {
         hours = document.getElementById("hoursInput").value;
         days = document.getElementById("daysInput").value;
+        let day = document.getElementById("daySelect");
         if (document.getElementById("firstColumn").childElementCount === 0) {
-            Draw(days, hours);
-
+            console.log(day.value);
+            Draw(days, hours, day.value);
         }
         return false;
     };
@@ -19,7 +22,7 @@ window.onload = function () {
     })
 };
 
-function Draw(nrDays, nrHours) {
+function Draw(nrDays, nrHours, day) {
     let newBlock;
     let days = document.getElementById('firstColumn');
     let hours = document.getElementById('firstRow');
@@ -27,13 +30,20 @@ function Draw(nrDays, nrHours) {
     let classHeight = (document.getElementById('content').clientHeight -
                         document.getElementById('content').clientHeight * 0.1) / nrDays;
     let hoursPx = (hours.clientWidth - hours.clientWidth * 0.1) / nrHours;
-
+    let count = dayList.indexOf(day);
+    console.log(count);
     // Any of the following "for" can be commented to better understand the code
     // Draw the rows where the day name will be written
     for (let i = 0; i < nrDays; i ++) {
         newBlock = document.createElement('div');
         newBlock.classList.add('days');
         newBlock.style.height = classHeight + 'px';
+        newBlock.setAttribute("id", dayList[count]);
+        newBlock.innerHTML = dayList[count];
+        count ++;
+        if (count > 6) {
+            count = 0;
+        }
         if (i === nrDays - 1) {
             newBlock.style.borderBottom = '0px';
         }
